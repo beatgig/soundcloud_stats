45eeba74070dfda01c9380375158be4f09df0baf
David Engelmann <david.engelmann44@gmail.com>
2025-03-05 12:24:53 -0500

    calculate stats from a soundcloud profile link

--- test/test_meta_stats.py
+++ test/test_meta_stats.py
@@ -1,15 +1,20 @@
-import soundcloud_stats
+import soundcloud_stats.auth
+import soundcloud_stats.account
 
 def test_soundcloud_stats():
-    soundcloud_client_id = soundcloud_stats.get_soundcloud_client_id()
-    soundcloud_client_secret = soundcloud_stats.get_soundcloud_client_secret()
+    soundcloud_client_id = soundcloud_stats.auth.get_soundcloud_client_id()
+    soundcloud_client_secret = soundcloud_stats.auth.get_soundcloud_client_secret()
     print(f"soundcloud client id: {soundcloud_client_id}")
     print(f"soundcloud client secret: {soundcloud_client_secret}")
     assert soundcloud_client_id
     assert soundcloud_client_secret
 
-    soundcloud_access_token = soundcloud_stats.get_soundcloud_access_token("https://secure.soundcloud.com/oauth/token", soundcloud_client_id, soundcloud_client_secret, "client_credentials")
+    soundcloud_access_token = soundcloud_stats.auth.get_soundcloud_access_token("https://secure.soundcloud.com/oauth/token", soundcloud_client_id, soundcloud_client_secret, "client_credentials")
     print(f"soundcloud access token: {soundcloud_access_token}")
     assert soundcloud_access_token
+
+    stats = soundcloud_stats.account.get_account_stats("https://soundcloud.com/chachiofficial", soundcloud_access_token, 10)
+    print(f"soundcloud stats: {stats}")
+    assert stats
     assert False
     
